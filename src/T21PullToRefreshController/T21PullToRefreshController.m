a82f3f29d28e3855f3e520e87b10f16c4fb35bb1
Eloi Guzman Ceron <guzman.eloi@gmail.com>
2017-02-22 18:29:19 +0100

    add queued operations so concatenated shows and hide do not interfere in the animation

--- src/T21PullToRefreshController/T21PullToRefreshController.m
+++ src/T21PullToRefreshController/T21PullToRefreshController.m
@@ -8,10 +8,29 @@
 
 #import "T21PullToRefreshController.h"
 
+typedef enum T21PullToRefreshOperationType {
+    T21PullToRefreshOperationTypeShow = 0,
+    T21PullToRefreshOperationTypeHide = 1
+}T21PullToRefreshOperationType;
+
+
+@interface T21PullToRefreshOperation : NSObject
+
+- (instancetype)initWithType:(T21PullToRefreshOperationType)type;
+
+@property (nonatomic) T21PullToRefreshOperationType type;
+@property (nonatomic) BOOL wasForcedProgramatically;
+
+@end
+
 @interface T21PullToRefreshControllerInternalState : NSObject
+
+-(void)addOperation:(T21PullToRefreshOperation*)newOperation;
 
 @property (nonatomic) NSNumber * isAnimating;
 @property (nonatomic,copy) void (^refreshBlock)();
+@property (nonatomic) NSMutableArray * queue;
+@property (nonatomic,weak) UIRefreshControl *refreshControl;
 
 @end
 
@@ -54,6 +73,7 @@
         
         T21PullToRefreshControllerInternalState * state = [[T21PullToRefreshControllerInternalState alloc] init];
         state.refreshBlock = refreshBlock;
+        state.refreshControl = refreshControl;
         
         [_refreshStates setObject:state forKey:refreshControl];
         [_refreshControls setObject:refreshControl forKey:scrollView];
@@ -87,21 +107,15 @@
 - (void) startPullToRefreshAnimation:(UIScrollView*)scrollView
 {
     UIRefreshControl *refreshControl = [_refreshControls objectForKey:scrollView];
-    [self animateRefreshControl:refreshControl wasForced:YES];
+    [self animateRefreshControl:refreshControl wasForcedProgramatically:YES];
 }
 
 - (void) finishPullToRefreshAnimation:(UIScrollView*)scrollView {
     UIRefreshControl *refreshControl = [_refreshControls objectForKey:scrollView];
     if (refreshControl) {
         T21PullToRefreshControllerInternalState * state = [_refreshStates objectForKey:refreshControl];
-        if (state && state.isAnimating.boolValue) {
-            // ugly trick to ensure the refresh control has time to be shown
-            // we found some issues showing the refresh control even after the viewDidLoad and viewWillAppear methods.
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-                state.isAnimating = @NO;
-                [refreshControl endRefreshing];
-            });
-        }
+        T21PullToRefreshOperation * operation = [[T21PullToRefreshOperation alloc] initWithType:T21PullToRefreshOperationTypeHide];
+        [state addOperation:operation];
     }
 }
 
@@ -115,36 +129,18 @@
     if (refreshControl) {
         T21PullToRefreshControllerInternalState * state = [_refreshStates objectForKey:refreshControl];
         if (state) {
-            return state.isAnimating.boolValue;
+            return state.queue.count > 0;
         }
     }
     return NO;
 }
 
-- (void) animateRefreshControl:(UIRefreshControl*)refreshControl wasForced:(BOOL)wasForced
+- (void) animateRefreshControl:(UIRefreshControl*)refreshControl wasForcedProgramatically:(BOOL)wasForcedProgramatically
 {
     T21PullToRefreshControllerInternalState * state = [_refreshStates objectForKey:refreshControl];
-    // ugly trick to ensure the refresh control has time to be shown
-    // we found some issues showing the refresh control even after the viewDidLoad and viewWillAppear methods.
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-        if (state && !state.isAnimating.boolValue) {
-            state.isAnimating = @YES;
-            if (wasForced) {
-                UIScrollView *scrollView = (UIScrollView*)refreshControl.superview;
-                if([scrollView isKindOfClass:[UIScrollView class]]){
-                    // UIViewControllers manage automatically the inset calculation when a refresh control begins
-                    // (taking in account the status bar and navigation bar)
-                    // In order to get the exact height of the refreshControl we inspect the contentInset increment
-                    CGFloat prevInset = scrollView.contentInset.top;
-                    [refreshControl beginRefreshing];
-                    
-                    //The beginRefreshing action doesn't scroll automatically to the refreshControl contentOffset
-                    CGFloat currentInset = scrollView.contentInset.top;
-                    [scrollView setContentOffset:CGPointMake(scrollView.contentOffset.x, scrollView.contentOffset.y - fabs(currentInset - prevInset)) animated:YES];
-                }
-            }
-        }
-    });
+    T21PullToRefreshOperation * operation = [[T21PullToRefreshOperation alloc] initWithType:T21PullToRefreshOperationTypeShow];
+    operation.wasForcedProgramatically = wasForcedProgramatically;
+    [state addOperation:operation];
 }
 
 #pragma mark - Pull to refresh by code
@@ -154,7 +150,7 @@
     UIRefreshControl *refreshControl = [_refreshControls objectForKey:scrollView];
     if (refreshControl)
     {
-        [self animateRefreshControl:refreshControl wasForced:YES];
+        [self animateRefreshControl:refreshControl wasForcedProgramatically:YES];
         [self executeRefreshBlock:refreshControl];
     }
 }
@@ -184,14 +180,26 @@
 {
     T21PullToRefreshControllerInternalState * state = [_refreshStates objectForKey:refreshControl];
     if (state) {
-        [self animateRefreshControl:refreshControl wasForced:NO];
+        [self animateRefreshControl:refreshControl wasForcedProgramatically:NO];
         [self executeRefreshBlock:refreshControl];
     }
 }
 
 @end
 
+@implementation T21PullToRefreshOperation
 
+- (instancetype)initWithType:(T21PullToRefreshOperationType)type
+{
+    self = [super init];
+    if (self) {
+        self.type = type;
+        self.wasForcedProgramatically = NO;
+    }
+    return self;
+}
+
+@end
 
 @implementation T21PullToRefreshControllerInternalState
 
@@ -201,8 +209,85 @@
     if (self) {
         self.isAnimating = @NO;
         self.refreshBlock = nil;
+        self.queue = [NSMutableArray array];
     }
     return self;
+}
+
+-(void)addOperation:(T21PullToRefreshOperation*)newOperation {
+    @synchronized (self) {
+        if (self.queue.count == 0) {
+            [self.queue addObject:newOperation];
+            [self executeOperation:newOperation];
+        } else {
+            T21PullToRefreshOperation * currentOperation = self.queue.firstObject;
+            if (self.queue.count == 1) {
+                // one action is already taking place: Show or Hide
+                // add if it's the opposite action
+                if (currentOperation.type != newOperation.type) {
+                    [self.queue addObject:newOperation];
+                }
+            } else {
+                //more than one action in the queue: Hide,Show or Show,Hide
+                if (currentOperation.type == newOperation.type) {
+                    // remove the opposite action and do not add the new action, as it's the same
+                    // Hide,Show <- Hide === Hide
+                    [self.queue removeObjectAtIndex:1];
+                } else {
+                    //noop: we are adding a redundant operation: Hide,Show <- Show === Hide,Show
+                }
+            }
+        }
+    }
+}
+
+-(void)executeOperation:(T21PullToRefreshOperation*)operation {
+    
+    if (operation.type == T21PullToRefreshOperationTypeShow) {
+        [self executeDelayed:0.2 block:^{
+            if (operation.wasForcedProgramatically) {
+                UIScrollView *scrollView = (UIScrollView*)self.refreshControl.superview;
+                if([scrollView isKindOfClass:[UIScrollView class]]){
+                    // UIViewControllers manage automatically the inset calculation when a refresh control begins
+                    // (taking in account the status bar and navigation bar)
+                    // In order to get the exact height of the refreshControl we inspect the contentInset increment
+                    CGFloat prevInset = scrollView.contentInset.top;
+                    [self.refreshControl beginRefreshing];
+                    
+                    //The beginRefreshing action doesn't scroll automatically to the refreshControl contentOffset
+                    CGFloat currentInset = scrollView.contentInset.top;
+                    [scrollView setContentOffset:CGPointMake(scrollView.contentOffset.x, scrollView.contentOffset.y - fabs(currentInset - prevInset)) animated:YES];
+                }
+            }
+            [self executeDelayed:0.5 block:^{
+                [self finishOperation];
+            }];
+        }];
+    } else if (operation.type == T21PullToRefreshOperationTypeHide) {
+        [self executeDelayed:0.2 block:^{
+            [self.refreshControl endRefreshing];
+            [self executeDelayed:0.5 block:^{
+                [self finishOperation];
+            }];
+        }];
+    }
+}
+
+-(void)executeDelayed:(CGFloat)seconds block:(void (^)())block {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, seconds * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        block();
+    });
+}
+
+-(void)finishOperation {
+    @synchronized (self) {
+        if (self.queue.count > 0) {
+            [self.queue removeObjectAtIndex:0];
+            if (self.queue.count > 0) {
+                [self executeOperation:self.queue.firstObject];
+            }
+        }
+    }
 }
 
 @end
